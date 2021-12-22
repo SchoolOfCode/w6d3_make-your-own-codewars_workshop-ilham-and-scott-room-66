18c2eb67dd5c71d283c9c8b9de6031af19412f73
scottwilson <swrs@hotmail.co.uk>
2021-12-22 16:28:46 +0000

    First commit, working with tests

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 /* 
 👉 Write your kata here!
 */
+//Make an array of objects, the task we want the victim to do is take the first 2 names from the array and insert them at the end of the array.
+export let array = [
+    "Chris", "Liz", "Arshi", "Jordan", "Jasien", "Chris M", "Dave", "Abdullah", "Hamza"
+];
+
+export function backOfTheLine (array){
+ let array2 = array.shift();
+ array.push(array2);
+ return array
+};
+
 
 //👉 Write the function your CodeWarriors will start with below here:
+
+// function backOfTheLine (array){
+//     //Write your code here...  
+// }
--- main.test.js
+++ main.test.js
@@ -1,1 +1,13 @@
 //👉 Write your tests below here:
+import { backOfTheLine, array } from "./main.js";
+
+// backOfTheLine()
+
+test("The fuction should return the same array but with Chris taken from the start and put into the end",  function(){
+const actual = backOfTheLine(array)
+const expected = [
+    "Liz", "Arshi", "Jordan", "Jasien", "Chris M", "Dave", "Abdullah", "Hamza", "Chris"
+]
+
+expect(actual).toStrictEqual(expected);
+});

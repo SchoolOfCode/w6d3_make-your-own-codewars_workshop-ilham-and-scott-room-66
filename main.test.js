eb7a55636fe7c77b1dc373051460e850cb037419
scottwilson <swrs@hotmail.co.uk>
2021-12-22 16:38:50 +0000

    finished codewars task and test

--- main.test.js
+++ main.test.js
@@ -1,12 +1,10 @@
 //👉 Write your tests below here:
 import { backOfTheLine, array } from "./main.js";
 
-// backOfTheLine()
-
 test("The fuction should return the same array but with Chris taken from the start and put into the end",  function(){
 const actual = backOfTheLine(array)
 const expected = [
-    "Liz", "Arshi", "Jordan", "Jasien", "Chris M", "Dave", "Abdullah", "Hamza", "Chris"
+    {name:"Liz"}, {name:"Arshi"}, {name:"Jordan"}, {name:"Jasien"}, {name:"Chris M"}, {name:"Dave"}, {name:"Abdullah"}, {name:"Hamza"}, {name: "Chris"}
 ]
 
 expect(actual).toStrictEqual(expected);
